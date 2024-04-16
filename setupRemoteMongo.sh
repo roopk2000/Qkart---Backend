36bd956363405f305fa2763c927971a8d0c9821a
Roop Kumar <108384154+roopk2000@users.noreply.github.com>
2024-04-16 23:11:28 +0530

    Commit By Roop

---
+++ setupRemoteMongo.sh
@@ -0,0 +1,3 @@
+# Setup file template to upload data to MongoDB Atlas
+mongoimport --uri "mongodb://ac-h0avtlu-shard-00-00.cchvfmr.mongodb.net:27017,ac-h0avtlu-shard-00-01.cchvfmr.mongodb.net:27017,ac-h0avtlu-shard-00-02.cchvfmr.mongodb.net:27017/?replicaSet=atlas-ecbc11-shard-0" --ssl --authenticationDatabase admin --username roopk2000 --password @1Roopkumar --drop --collection users --file data/export_qkart_users.json
+mongoimport --uri "mongodb://ac-h0avtlu-shard-00-00.cchvfmr.mongodb.net:27017,ac-h0avtlu-shard-00-01.cchvfmr.mongodb.net:27017,ac-h0avtlu-shard-00-02.cchvfmr.mongodb.net:27017/?replicaSet=atlas-ecbc11-shard-0" --ssl --authenticationDatabase admin --username roopk2000 --password @1Roopkumar --drop --collection products --file data/export_qkart_products.json
